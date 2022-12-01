e54086eba76c50cc6581474ac753d66c68b7d457
sioux.dias@gmail.com <sioux.dias@gmail.com>
2022-12-01 06:07:17 -0300

    limpeza no cod do menu

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-var menu = document.querySelector('.menu');
-var body = document.querySelector('body');
 var html = document.querySelector('html');
-var eclipse = document.querySelector('.eclipse');
-
-
 
 function menuAlterar(){
-  menu.classList.toggle('menu--active');
-    
-  eclipse.classList.toggle('eclipse--active');
   html.classList.toggle('menu-active');
-  
 }
 
