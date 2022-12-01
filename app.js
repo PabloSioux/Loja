d8b6f14aa609ca9ccb434440a0c477b2b886a7af
sioux.dias@gmail.com <sioux.dias@gmail.com>
2022-12-01 06:37:21 -0300

    comecei o aside carrinho

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ function menuAlterar(){
   html.classList.toggle('menu-active');
 }
 
+function carrinhoAlterar(){
+  html.classList.toggle('carrinho-active');
+}
+
+function fecharAside() {
+  if(html.classList.contains('menu-active')){
+    menuAlterar();
+  } else {
+    carrinhoAlterar();
+  }
+}
