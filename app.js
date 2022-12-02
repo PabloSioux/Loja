141374c14dfb67301f44f323c23268c72761eeca
Pablo Sioux <sioux.dias@gmail.com>
2022-12-02 11:10:36 -0300

    hack para concertar o vh
    usando javascript

--- app.js
+++ app.js
@@ -21,3 +21,11 @@ function fecharAside() {
 }
 
 
+
+
+const documentHeight = () => {
+  const doc = document.documentElement;
+  doc.style.setProperty('--doc-height', `${window.innerHeight}px`);
+ }
+ window.addEventListener('resize', documentHeight);
+ documentHeight();
